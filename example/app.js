1801c103c76c58d860612e9d088b60f3ecc6ca8d
Patrick Treyer <p.treyer@web.de>
2016-11-16 11:11:19 +0100

    added ppt, example code and the url to plunker

---
+++ example/app.js
@@ -0,0 +1,32 @@
+var myApp = angular.module('myApp', []);
+
+myApp.controller('myController', ['$scope', function($scope) {
+
+            $scope.records = [
+                   {
+                        "name" : "Mathematik",
+                        "semester" : 2,
+                        "note" : 1.4
+                    },{
+                        "name" : "Compilerbau",
+                        "semester" : 3,
+                        "note" : 1.4
+                    },{
+                        "name" : "Rechnerarchitektur",
+                        "semester" : 3,
+                        "note" : 2.2
+                    }
+                ]
+                
+             $scope.addKlausur = function() {
+                
+                var record = $scope.klausur;
+                
+                $scope.records.push(record);
+                
+                $scope.klausur = '';
+                
+            };    
+
+} ]);
+
